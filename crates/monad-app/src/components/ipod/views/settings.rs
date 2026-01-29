064638f76947a8ff0d5be51e04a7a29e60ad465a
ayush-that <ayush1337@hotmail.com>
2026-01-29 16:25:57 +0530

    remove mute toggle from settings since it doesn't work

--- crates/monad-app/src/components/ipod/views/settings.rs
+++ crates/monad-app/src/components/ipod/views/settings.rs
@@ -3,9 +3,9 @@
 use dioxus::prelude::*;
 
 use crate::services::AudioService;
+use crate::state::AppState;
 use crate::state::ipod::{ColorTheme, IPodState};
 use crate::state::player::RepeatMode;
-use crate::state::AppState;
 
 /// Settings view with theme and playback options.
 #[component]
@@ -152,41 +152,13 @@ fn VolumeControl() -> Element {
     let app_state = use_context::<AppState>();
     let audio = use_context::<Signal<AudioService>>();
     let volume = *app_state.player.volume.read();
-    let muted = *app_state.player.muted.read();
 
-    let display_volume = if muted { 0.0 } else { volume };
-    let volume_percent = (display_volume * 100.0) as i32;
+    let volume_percent = (volume * 100.0) as i32;
 
-    // Get the signals we need to mutate
     let mut player_volume = app_state.player.volume;
-    let mut player_muted = app_state.player.muted;
 
     rsx! {
         div { class: "ipod-settings__volume-control",
-            // Mute toggle
-            div {
-                class: "ipod-settings__mute-btn",
-                onclick: move |_| {
-                    let is_muted = *player_muted.read();
-                    *player_muted.write() = !is_muted;
-                },
-                svg {
-                    width: "24",
-                    height: "24",
-                    view_box: "0 0 24 24",
-                    fill: if muted { "#ef4444" } else { "#fff" },
-                    if muted {
-                        path {
-                            d: "M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"
-                        }
-                    } else {
-                        path {
-                            d: "M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"
-                        }
-                    }
-                }
-            }
-
             // Volume slider
             div { class: "ipod-settings__volume-slider",
                 // Decrease button
@@ -206,7 +178,7 @@ fn VolumeControl() -> Element {
                     div { class: "ipod-settings__volume-bar",
                         div {
                             class: "ipod-settings__volume-fill",
-                            style: "width: {display_volume * 100.0}%"
+                            style: "width: {volume * 100.0}%"
                         }
                     }
                     span { class: "ipod-settings__volume-text", "{volume_percent}%" }
